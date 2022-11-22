990b164eb9ca56e8c94daa941271a6120f01bdb2
Taeseo Moon / 문태서 <98258284+ansxotj06@users.noreply.github.com>
2022-11-22 15:57:30 +0900

    Create index.js

---
+++ [WEB]/WebGame Lecture/1st lecture/index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Canvas tutorial</title>
+</head>
+
+<body>
+    <canvas id = "canvas" width = "1000" height="1000"></canvas>
+    <script src="back.js"></script>
+</body>
+
+</html>
