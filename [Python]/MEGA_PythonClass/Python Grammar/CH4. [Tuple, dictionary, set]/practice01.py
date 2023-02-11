e6afc2697fecde581a103d83fd4abaac51603ed5
Taeseo Moon / 문태서 <mts060917@gmail.com>
2023-02-12 00:08:04 +0900

    Add files via upload

---
+++ [Python]/MEGA_PythonClass/Python Grammar/CH4. [Tuple, dictionary, set]/practice01.py
@@ -0,0 +1,117 @@
+# tu1 = (10, 20, 30)
+# print(tu1[1])
+# print(len(tu1))
+# print(10 in tu1)
+#
+# tu2 = ('forty', 'fifty')
+# tu3 = tu1 + tu2
+# print(tu3)
+# print(tu1 * 3)
+#
+# tu1 = (1, 5, 3)
+# tu1 = tu1[0:1] + (2, ) + tu1[2: ]
+# print(tu1)
+#
+# tu2 = (1, 5, 3)
+# tu2 = (tu1[0], 2, tu1[2])
+# print(tu2)
+#
+# tu1 = (1, 5, 3)
+# tu1 += (4,)
+# print(tu1)
+#
+# tu1 = (1, 5, 3)
+# tu1 = tu1[1 : ]
+# print(tu1)
+#
+# tu1 = (1, 5, 3)
+# tu1 = (tu1[0], tu1[2])
+# print(tu1)
+
+# menu = ('짜장면', '우동', '짬뽕', '볶음밥')
+# print(menu)
+# print(menu[0])
+# print(menu[1 : ])
+
+# tuTest = ('가', '나', '다')
+# tuTest += ('라', '마')
+# print(len(tuTest))
+# print('다' in tuTest)
+
+# a = {'name': '어벤져스', 'type': '히어로 무비'}
+# print(a.keys())
+#
+# d = {'a': '사과', 'b': '바나나'}
+# d.update(a = '수바ㅏ앙아악')
+# print(d)
+
+# dct = {"name": "망고", "price": 6000, "origin": "필리핀"}
+#
+# for key, value in dct.items():
+#     print(key, dct[key])
+#
+# me = {'키': 120,
+#       '성격': '활발',
+#       '몸무게': 23,
+#       '나이': 8,
+#       '성별': '남'}
+#
+# for value in me.values():
+#     print(value)
+
+# ice = {"메로나": 1000, "폴라포": 1200, "빵빠레": 1800}
+# ice.update(죠스바 = 1200, 월드콘 = 1500)
+# ice.update(메로나 = 1300)
+# del ice['메로나']
+# print(ice)
+
+# ice = {"메로나": [300, 20], "비비빅": [400, 3], "죠스바": [250, 100]}
+# print(ice)
+
+# phones = {"a": 1, "b": 2, "c": 3, "d": 4}
+# print(phones.pop('d'))
+
+# ice = {'탱크보이':1200, "폴라포": 900, '월드콘': 1500, '메로나': 1000}
+# list = list()
+# for value in ice.keys():
+#     list.append(value)
+# print(list)
+#
+# ice = {'탱크보이': 1200, '폴라포': 900, '월드콘': 1500, '메로나': 1000}
+# sum = 0
+# for value in ice.values():
+#     sum += value
+# print(sum)
+
+# Warriior = {'이름': 'Warrior',
+#             '힘': 88,
+#             '마법': 65,
+#             '민첩': 80,
+#             '체력': 90}
+# Warriior.update(체력 = Warriior['체력'] - 30)
+# Warriior.update(방어 = 70)
+# for key, value in Warriior.items():
+#     print(key, value)
+
+# ad = {'id': "admin", 'password': "12345"}
+# input_id = input("아이디를 입력하세요 : ")
+# input_pw = input("비밀번호를 입력하세요 : ")
+# if input_id == ad['id'] and input_pw == ad['password']:
+#     print("모든 정보에 접근 권한이 있습니다.")
+# else:
+#     print("정보에 접근 권한이 없습니다!")
+
+
+# score = {"김채린": 85,
+#          "박수정": 98,
+#          "함소희": 94,
+#          "안예린": 90,
+#          "연수진": 93}
+# sum = 0
+
+# b = {1, 2, 300, 3, 30}
+# print(b)
+# a = {"hello", 'dkd', "dkd", 'aaa'}
+# print(a)
+# print(type(a))
+
