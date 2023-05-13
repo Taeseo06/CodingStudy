e0ad589ccad132e31268f65156847a9cc3dadf45
Taeseo Moon / 문태서 <mts060917@gmail.com>
2023-05-13 11:55:07 +0900

    Create vpython stydy01.py
    study update

---
+++ [Python]/VPython/vpython stydy01.py
@@ -0,0 +1,53 @@
+Web VPython 3.2
+
+
+
+
+#myBall = sphere(color = color.red, radius = 2)
+#myBox = box(pos = vec(5, 0, 0), color = color.blue, size = vec(0.5, 4, 1))
+#myBall.color = color.green
+#myBox.pos.x = 10
+#
+#myCylinder = cylinder(size = vec(3, 1, 3))
+#
+#r = vector(3, -4, 5)
+#r_arrow = arrow(pos = vector(0, 0, 0), axis = r, shaftwidth = 0.2)
+
+#
+#x = vector(10, 0, 0)
+#y = vector(0, 10, 0)
+#z = vector(0, 0, 10)
+#
+#x_arrow = arrow(axis = x, shaftwidth = 0.1)
+#y_arrow = arrow(axis = y, shaftwidth = 0.1)
+#z_arrow = arrow(axis = z, shaftwidth = 0.1)
+
+
+#r = vector(6, 8, 0)
+#r_mag = mag(r)
+#r_hat = norm(r)
+#r_hat_mag = mag(r_hat)
+#r_arrow = arrow(pos = vec(-5, 0, 0), axis = r, color = color.red, shaftwidth = 0.2)
+#r_hat_arrow = arrow(axis = r_hat,
+#color = color.cyan,
+#shaftwidth = 0.2)
+#print("r: ", r)
+#print("r_mag: ", r_mag)
+#print("r_hat_mag: " ,r_hat_mag)
+#print("r_hat: ", r_hat)
+
+#a = vector(3, 4, 0)
+#b = vector(5, 1, 0)
+#a_arrow = arrow(pos = vector(0, 0, 0), axis = a, shaftwidth = 0.2, color = color.blue)
+#b_arrow = arrow(pos = vector(3, 4, 0), axis = b, shaftwidth = 0.2, color = color.green)
+#c = a + b
+#c_arrow = arrow(pos = vector(0, 0, 0), axis = c, shaftwidth = 0.2, color = color.red)
+
+#a = vector(3, 4, 0)
+#b = vector(5, 1, 0)
+#a_arrow = arrow(pos = vector(0, 0, 0), axis = a, shaftwidth = 0.2, color = color.blue)
+#b_arrow = arrow(pos = vector(0, 0, 0), axis = b, shaftwidth = 0.2, color = color.green)
+#d = a - b
+#d_arrow = arrow(pos = b, axis = d, 
+#shaftwidth = 0.2, color = color.red)
+
